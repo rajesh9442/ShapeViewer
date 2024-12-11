2bfa83a10cdad816f1f7186981b1cf1296310400
RajeshMahendran <rajeshmahendran1999@gmail.com>
2024-12-10 20:17:11 -0500

    Remove unused elements

--- src/components/LeftMenu.js
+++ src/components/LeftMenu.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const LeftMenu = ({ onFileOpen }) => {
   const [uploadedFiles, setUploadedFiles] = useState([]); // Store the list of uploaded files
